5ae45fc36e2c7b94ef51409fb2e42fa0bf6800c9
Marviuz <marviuz.gaming@gmail.com>
2023-05-13 11:27:20 +0800

    modify TextField component

--- src/components/base/TextField/index.tsx
+++ src/components/base/TextField/index.tsx
@@ -1,16 +1,21 @@
-import { ComponentProps, FC } from 'react';
+import { ComponentProps, FC, PropsWithRef, forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export type TextFieldProps = ComponentProps<'input'> & {};
+export type TextFieldProps = PropsWithRef<ComponentProps<'input'> & {}>;
 
-export const TextField: FC<TextFieldProps> = ({ className, ...props }) => {
-  return (
-    <input
-      className={twMerge(
-        '"px-4 py-2 text-white transition-colors bg-transparent border rounded outline-none border-secondary focus:bg-secondary"',
-        className
-      )}
-      {...props}
-    />
-  );
-};
+export const TextField: FC<TextFieldProps> = forwardRef(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        className={twMerge(
+          '"px-4 py-2 text-white transition-colors bg-transparent border rounded outline-none border-secondary focus:bg-secondary"',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+TextField.displayName = TextField.name;
