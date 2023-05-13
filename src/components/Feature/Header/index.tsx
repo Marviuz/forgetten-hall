3b225292d27640d7cafb7ba69ce877080abfeb60
Marviuz <marviuz.gaming@gmail.com>
2023-05-13 17:53:47 +0800

    add add data card to header

--- src/components/Feature/Header/index.tsx
+++ src/components/Feature/Header/index.tsx
@@ -1,8 +1,14 @@
+'use client';
+
+import { Button } from '@/components/base/Button';
+import { Dialog, Transition } from '@headlessui/react';
 import { ImagePaths } from '@/constants/ImagePaths';
 import { NavigationRoutes } from '@/constants/NavigationRoutes';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useState, Fragment } from 'react';
+import { AddDataCard } from '../AddDataCard';
+import CHARACTERS from '@/constants/characters';
 
 export type HeaderProps = {};
 
@@ -22,25 +28,70 @@ const calculateApproximateSize = (height: number) => {
 };
 
 export const Header: FC<HeaderProps> = () => {
+  let [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="h-20 px-4 border-b bg-primary-1 border-secondary">
-      <div className="w-full h-full">
-        <div className="flex items-center w-full h-full">
-          <Link
-            href={`${NavigationRoutes.Home}`}
-            className="flex items-center italic"
-          >
-            <Image
-              src={ImagePaths.StarRailLogo}
-              alt="Honkai Star Rail Logo"
-              {...calculateApproximateSize(64)}
-            ></Image>
-            <div className="text-3xl font-bold text-white">
-              <span className="text-accent">Forgotten</span> Hall
+    <>
+      <header className="h-20 px-4 border-b bg-primary-1 border-secondary">
+        <div className="w-full h-full">
+          <div className="flex h-full">
+            <div className="flex items-center w-full h-full">
+              <Link
+                href={`${NavigationRoutes.Home}`}
+                className="flex items-center italic"
+              >
+                <Image
+                  src={ImagePaths.StarRailLogo}
+                  alt="Honkai Star Rail Logo"
+                  {...calculateApproximateSize(64)}
+                ></Image>
+                <div className="text-3xl font-bold text-white">
+                  <span className="text-accent">Forgotten</span> Hall
+                </div>
+              </Link>
             </div>
-          </Link>
+            <div className="flex items-center">
+              <Button onClick={() => setIsOpen(true)}>HAKDOG</Button>
+            </div>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <Transition show={isOpen} as={Fragment}>
+        <Dialog onClose={() => setIsOpen(false)}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/30" />
+          </Transition.Child>
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[1440px]">
+              <AddDataCard
+                onSubmit={console.log}
+                options={CHARACTERS.map(({ name, img }) => ({
+                  value: name,
+                  avatar: img,
+                }))}
+              />
+            </Dialog.Panel>
+          </Transition.Child>
+        </Dialog>
+      </Transition>
+    </>
   );
 };
