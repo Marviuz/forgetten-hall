680b3489389b2247f9c5ecf2036b22f8cc867a3e
Marviuz <marviuz.gaming@gmail.com>
2023-05-13 16:50:16 +0800

    add character db

---
+++ src/constants/characters.ts
@@ -0,0 +1,34 @@
+const CHARACTERS = [
+  { name: 'Arlan', img: '/images/characters/Arlan.png' },
+  { name: 'Asta', img: '/images/characters/Asta.png' },
+  { name: 'Bailu', img: '/images/characters/Bailu.png' },
+  { name: 'Bronya', img: '/images/characters/Bronya.png' },
+  { name: 'Clara', img: '/images/characters/Clara.png' },
+  { name: 'Dan Heng', img: '/images/characters/Dan Heng.png' },
+  { name: 'Gepard', img: '/images/characters/Gepard.png' },
+  { name: 'Herta', img: '/images/characters/Herta.png' },
+  { name: 'Himeko', img: '/images/characters/Himeko.png' },
+  { name: 'Hook', img: '/images/characters/Hook.png' },
+  // { name: 'Jing Yuan', img: '/images/characters/Jing Yuan.png' },
+  { name: 'March 7th', img: '/images/characters/March 7th.png' },
+  { name: 'Natasha', img: '/images/characters/Natasha.png' },
+  { name: 'Pela', img: '/images/characters/Pela.png' },
+  { name: 'Qingque', img: '/images/characters/Qingque.png' },
+  { name: 'Sampo', img: '/images/characters/Sampo.png' },
+  { name: 'Seele', img: '/images/characters/Seele.png' },
+  { name: 'Serval', img: '/images/characters/Serval.png' },
+  { name: 'Sushang', img: '/images/characters/Sushang.png' },
+  { name: 'Tingyun', img: '/images/characters/Tingyun.png' },
+  {
+    name: 'Trailblazer (Physical)',
+    img: '/images/characters/Trailblazer (Physical).png',
+  },
+  {
+    name: 'Trailblazer (Fire)',
+    img: '/images/characters/Trailblazer (Fire).png',
+  },
+  { name: 'Welt', img: '/images/characters/Welt.png' },
+  { name: 'Yanqing', img: '/images/characters/Yanqing.png' },
+];
+
+export default CHARACTERS;
