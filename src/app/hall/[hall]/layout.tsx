8473ff5c201ee31c657b33cfb38eeda7805f636f
Marviuz <marviuz.gaming@gmail.com>
2023-05-14 22:35:22 +0800

    fix meta data issue for now

--- src/app/hall/[hall]/layout.tsx
+++ src/app/hall/[hall]/layout.tsx
@@ -1,16 +1,6 @@
 import { ReactNode } from 'react';
 
-type GenerateMetadataProps = {
-  params: { hall: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export const generateMetadata = ({ params }: GenerateMetadataProps) => {
-  // TODO something better
-  return {
-    title: `Hall ${params.hall} stats!`,
-  };
-};
+// TODO add dynamic meta data based on current hall#
 
 export default function HallLayout({ children }: { children: ReactNode }) {
   return children;
