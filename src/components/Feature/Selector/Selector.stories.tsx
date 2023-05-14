e7dc0f19c6bc9f8bc48148c2e6aeb0f5f145e328
Marviuz <marviuz.gaming@gmail.com>
2023-05-14 11:24:39 +0800

    add selector component

---
+++ src/components/Feature/Selector/Selector.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Selector } from '.';
+
+const meta: Meta<typeof Selector> = {
+  title: 'Feature/Selector',
+  component: Selector,
+};
+
+export default meta;
+type Story = StoryObj<typeof Selector>;
+
+export const Default: Story = {
+  args: {
+    value: {
+      src: 'https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg',
+      value: 'sample',
+      label: 'Sample',
+    },
+    options: Array.from(Array(6)).map((_, i) => ({
+      src: 'https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg',
+      value: 'sample',
+      label: `Sample ${i}`,
+    })),
+  },
+};
---
+++ src/components/Feature/Selector/index.tsx
@@ -0,0 +1,108 @@
+import { Button } from '@/components/base/Button';
+import { Dialog, Transition } from '@headlessui/react';
+import Image, { ImageProps } from 'next/image';
+import { FC, useState, Fragment } from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+
+export type ValueType = {
+  id?: string;
+  src: ImageProps['src'];
+  value: string;
+  label: string;
+};
+
+export type SelectorProps = {
+  value?: ValueType;
+  onChange: (value: ValueType) => void;
+  name: string;
+  options: ValueType[];
+  disabled?: boolean;
+};
+
+export const Selector: FC<SelectorProps> = ({
+  value,
+  options,
+  onChange,
+  disabled,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        className="relative items-end p-0 overflow-hidden bg-center bg-cover enabled:active:scale-100 enabled:hover:scale-105 w-36 h-52 disabled:bg-gray-500 disabled:bg-blend-multiply"
+        style={{ backgroundImage: value?.src && `url(${value.src})` }}
+        onClick={() => setIsOpen(true)}
+        disabled={disabled}
+      >
+        {value ? (
+          <div className="w-full text-white bg-black/75">
+            <div className="p-3 text-lg font-semibold text-center">
+              {value.label}
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center w-full h-full text-white">
+            <AiOutlinePlus className="text-6xl text-gray-600" />
+          </div>
+        )}
+      </Button>
+
+      <Transition show={isOpen} as={Fragment}>
+        <Dialog onClose={() => setIsOpen(false)}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/30" />
+          </Transition.Child>
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel className="absolute text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-full max-w-[888px] bg-primary-1 border border-secondary rounded">
+              <div className="p-8">
+                <div className="flex flex-wrap justify-center gap-2">
+                  {options.map((val, i) => (
+                    <Button
+                      key={val.id || i}
+                      className="relative items-end p-0 overflow-hidden active:scale-100 hover:scale-105 w-36 h-52"
+                      onClick={() => {
+                        onChange(val);
+                        setIsOpen(false);
+                      }}
+                    >
+                      <div className="z-10 w-full text-white bg-black/75">
+                        <div className="p-3 text-lg font-semibold text-center">
+                          {val.label}
+                        </div>
+                      </div>
+
+                      <Image
+                        src={val.src}
+                        alt={val.label}
+                        fill
+                        className="absolute top-0 left-0 object-cover w-full h-full pointer-events-none"
+                      />
+                    </Button>
+                  ))}
+                </div>
+              </div>
+            </Dialog.Panel>
+          </Transition.Child>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
