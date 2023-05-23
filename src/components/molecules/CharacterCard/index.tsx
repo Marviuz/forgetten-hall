d6d6d6c3204ce6bf68e6203210f022990a850a28
Marviuz <marviuz.gaming@gmail.com>
2023-05-24 06:00:49 +0800

    modify styles of character chard

--- src/components/molecules/CharacterCard/index.tsx
+++ src/components/molecules/CharacterCard/index.tsx
@@ -16,15 +16,15 @@ export const CharacterCard: FC<CharacterCardProps> = ({ image, name }) => {
         src={image}
         alt={name}
         fill
-        className="top-0 left-0 w-full h-full pointer-events-none"
+        className="top-0 left-0 object-cover w-full h-full pointer-events-none"
       />
-      <div className="absolute bottom-0 left-0 w-full p-8 bg-black/70 h-1/3">
+      <div className="absolute bottom-0 left-0 w-full p-4 bg-black/70 h-1/3">
         <div className="flex items-center justify-center w-full h-full">
           <ToolTip
             render={(id) => (
               <Text
                 variant="h4"
-                className="font-bold text-center truncate"
+                className="leading-tight tracking-tight text-center break-words line-clamp-2"
                 data-tooltip-id={id}
               >
                 {name}
